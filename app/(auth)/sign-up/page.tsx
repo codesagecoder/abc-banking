c977c5eb046db0a0d96e08e381ddce8b68f9be44
codesage <laughingsagecoder@gmail.com>
2024-05-10 19:03:35 +0200

    implement initial file and folder structure

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return (
+    <div>Sign up</div>
+  );
+}
+
+export default Page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+
+const Page = () => {
+  return (
+    <div>Home</div>
+  );
+}
+
+export default Page;
