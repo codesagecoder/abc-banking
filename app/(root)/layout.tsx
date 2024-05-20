45d4de298363b3ac5709cd8a671663600ee82ad4
codesage <laughingsagecoder@gmail.com>
2024-05-20 21:31:53 +0200

    building the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Guest', lastName: 'Account' };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
